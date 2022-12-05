49dff721af07b13a883fd62415dfa56e0750fcdc
YASHsingh06 <119647446+YASHsingh06@users.noreply.github.com>
2022-12-05 17:39:01 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,21 @@
+# a=list(input().split(","))
+# b=list(map(int,input().split(",")))
+# d=(dict(zip(a,b)))
+# print(str(d))
+# t=[]
+# r=dict()
+# for k,v in d.items():
+#     if v not in t:
+#         t.append(v)
+#         r[k]=v
+# print(str(r))
+
+
+# x=int(input())
+# y=int(input())
+# z=int(input())
+# print(list([b,c,d] for b in range (x+1) for c in range(y+1) for d in range (z+1) if (x+y+z!=3)))
+
+
+
+
